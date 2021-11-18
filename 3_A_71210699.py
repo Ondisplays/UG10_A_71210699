fc7c095c22f8cca4b78edf0ff86921d16ad63941
Ondisplays <93306165+Ondisplays@users.noreply.github.com>
2021-11-18 16:02:39 +0700

    Create 3_A_71210699.py
    3_A_71210699

---
+++ 3_A_71210699.py
@@ -0,0 +1,14 @@
+#input
+
+skuy = input("Mendatar/Menurun?: ")
+
+#hasil
+
+if skuy == "Mendatar":
+    skidi = int(input("Masukan kolom: "))
+    print("#" * skidi)
+elif skuy == "Menurun":
+    pu = int(input("Masukan baris: "))
+    print("*\n" * pu)
+else:
+    print("Pola tidak dikenali")
